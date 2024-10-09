e788a0c485b71b85eb8e3934a96f0fb52af7e0fc
Adam Wozniak <adamwozhere@gmail.com>
2024-10-09 16:51:18 +0100

    arrays sumAllTails

--- 04-arrays/sum.go
+++ 04-arrays/sum.go
@@ -23,6 +23,8 @@ package main
 // Step 3. Refactor
 // Notice we have changed to using a Slice now `[]int`,
 // so that we can have an arbitrary size of the numbers array input
+
+// Sum returns all the numbers in a slice added together.
 func Sum(numbers []int) int {
 	sum := 0
 	// `range` iterator returns an `index` and `value`
@@ -31,4 +33,75 @@ func Sum(numbers []int) int {
 		sum += number
 	}
 	return sum
+}
+
+// input any number of integer arrays, and return an integer array with the sum of each array
+// Step 1. Minimum code
+// func SumAll(numbersToSum ...[]int) []int {
+// 	return nil
+// }
+
+// Step 2. Make it pass
+// func SumAll(numbersToSum ...[]int) []int {
+// 	lengthOfNumbers := len(numbersToSum)
+
+// 	// use `make` to create a slice with initial length of `lengthOfNumbers`
+// 	// The length is the number of elements it holds,
+// 	// while the capacity is the number of elements it can hold in the underylying array `cap(mySlice)`,
+// 	// e.g. `make([]int, 0, 5)` creates a slice with length 0 and capacity 5.
+
+// 	sums := make([]int, lengthOfNumbers)
+
+// 	// loop through the range, putting the sum into the index of the sums array
+
+// 	for i, numbers := range numbersToSum {
+// 		sums[i] = Sum(numbers)
+// 	}
+
+// 	return sums
+// }
+
+// Step 3. Refactor
+// In the previous example we used the index, which could give a runtime error if index is out of range.
+// Now we use the `append` function which takes a slice and a new value,
+// then returns a new slice with all the items in it.
+// In this implementation we don't worry about capacity, we start with an empty slice `sums` and append to it.
+// func SumAll(numbersToSum ...[]int) []int {
+// 	var sums []int
+// 	for _, numbers := range numbersToSum {
+// 		sums = append(sums, Sum(numbers))
+// 	}
+
+// 	return sums
+// }
+
+// New requirement is to change SumAll to SumAllTails, where it will calculate the totals of the "tails"
+// of each slice. The tail of a collection is all items in the collection except the first one (the "head")
+
+// func SumAllTails(numbersToSum ...[]int) []int {
+// 	var sums []int
+// 	for _, numbers := range numbersToSum {
+// 		// slices can be sliced with syntax `slice[low:high]`.
+// 		// ommitting the value on one side means it will take everything on that side.
+// 		// here we are saying "take from 1 to the end" (leaves off index 0)
+// 		tail := numbers[1:]
+// 		sums = append(sums, Sum(tail))
+// 	}
+
+// 	return sums
+// }
+
+// rewrite the function so that it passes if an empty slice is passed
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }
--- 04-arrays/sum_test.go
+++ 04-arrays/sum_test.go
@@ -7,7 +7,10 @@ package main
 // and hence Go will not allow you to import a package with name `main` -
 // therefore rename the main module to something else e.g. `go mod init arrays`.
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	// We had two tests, for 5 numbers and 3 numbers,
@@ -29,4 +32,59 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+// func TestSumAll(t *testing.T) {
+// 	got := SumAll([]int{1, 2}, []int{0, 9})
+// 	want := []int{3, 9}
+
+// 	// when the initial Step 1. function returns nil, the compiler errors:
+// 	// invalid operation: got != want (slice can only be compared to nil).
+// 	// Go does not let you use equality operators with slices.
+// 	// You could write a function to iterate each slice, but we can use `reflect.DeepEqual` instead.
+
+// 	// if got != want {
+// 	// 	t.Errorf("got %v want %v", got, want)
+// 	// }
+
+// 	// now the test will compile and run because we are using `DeepEqual`,
+// 	// which is useful for seeing if ANY two variables are the same.
+// 	// Note that `DeepEqual` is NOT type safe. The code will compile if we changed `got` to a string etc.
+
+// 	if !reflect.DeepEqual(got, want) {
+// 		t.Errorf("got %v want %v", got, want)
+// 	}
+
+// 	// Grom Go 1.21 slices standard packages has `slices.Equal` to do a simple shallow compare on slices,
+// 	// where you don't need to worry about the types. Note that this function expects elements to be comparable,
+// 	// so it can't be applied to slices with non-comparable elements like 2D slices.
+// }
+
+func TestSumAllTails(t *testing.T) {
+	// reducing the repeated code, this time by assigning a function to a variable,
+	// this technique can be useful when you want to bind a function to other local variables in scope.
+	// it also allows you to reduce the surface area of your API, as it cannot be used outside of this test.
+	// It gives type safety too as you cannot create a new test with checkSums
+	// Hiding variables and functions that don't need to be exported is an important design consideration.
+	checkSums := func(t testing.TB, got, want []int) {
+		// here I am not sure why `t testing.TB` does not include the asterisk
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	// initially this test will compile but have a runtime error, as index is out of bounds,
+	// because we are trying to access index 1 on and empty slice (with [1:])
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
 }
